37935853ca0e897caccab398053b92263970a855
niiyazobug <niiyaz5555@gmail.com>
2022-04-11 10:49:11 +0600

    data fetch in services and service component added

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 
 const Services = () => {
+
+    const [services, setServices] = useState([])
+
+
+    useEffect(() => {
+
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+
+    }, [])
+
     return (
         <div>
-            <h2>services</h2>
+            <h2>services: {services.length}</h2>
         </div>
     );
 };

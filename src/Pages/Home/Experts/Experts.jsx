eecbd8caf54f421bdb1fa5bad3c3ad5bb78569dc
niiyazobug <niiyaz5555@gmail.com>
2022-04-11 13:49:22 +0600

    map for expert

--- src/Pages/Home/Experts/Experts.jsx
+++ src/Pages/Home/Experts/Experts.jsx
@@ -6,22 +6,32 @@ import expert3 from '../../../Images/Experts/expert-3.jpg'
 import expert4 from '../../../Images/Experts/expert-4.jpg'
 import expert5 from '../../../Images/Experts/expert-5.jpg'
 import expert6 from '../../../Images/Experts/expert-6.png'
+import Expert from '../Expert/Expert';
 
-const expert = [
-    {id: 1, name: 'Will Smith', img: expert1},
-    {id: 2, name: 'Chris Rock', img: expert2},
-    {id: 3, name: 'Dwayne Rock', img: expert3},
-    {id: 4, name: 'Messy Vai', img: expert4},
-    {id: 5, name: 'Ronaldo Bro', img: expert5},
-    {id: 6, name: 'Stachy Jhonson', img: expert6},
+const experts = [
+    { id: 1, name: 'Will Smith', img: expert1 },
+    { id: 2, name: 'Chris Rock', img: expert2 },
+    { id: 3, name: 'Dwayne Rock', img: expert3 },
+    { id: 4, name: 'Messy Vai', img: expert4 },
+    { id: 5, name: 'Ronaldo Bro', img: expert5 },
+    { id: 6, name: 'Stachy Jhonson', img: expert6 },
 ]
 
 const Experts = () => {
 
 
     return (
-        <div>
+        <div className='container'>
             <h2 className=''>Our Experts</h2>
+
+            <div className='row'>
+                {
+                    experts.map(expert => <Expert
+                        key={expert.id}
+                        expert={expert}
+                    ></Expert>)
+                }
+            </div>
         </div>
     );
 };

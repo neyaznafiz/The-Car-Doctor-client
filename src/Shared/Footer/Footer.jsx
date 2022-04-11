33320d51514be0423834a817c3430836fa2813bf
niiyazobug <niiyaz5555@gmail.com>
2022-04-11 10:00:01 +0600

    components created

---
+++ src/Shared/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            {/* year ta dynamic boshaite hobe */}
+            <p> <small>Copyright © 2022 neyaznafiz</small></p>
+        </footer>
+    );
+};
+
+export default Footer;

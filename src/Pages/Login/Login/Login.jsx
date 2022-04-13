9893a34b40b70fe070ea7e74edd1087c8ee91f55
niiyazobug <niiyaz5555@gmail.com>
2022-04-13 13:43:41 +0600

    useRef and handle submit function added in form

--- src/Pages/Login/Login/Login.jsx
+++ src/Pages/Login/Login/Login.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
 
 const Login = () => {
+
+    const emailRef = useRef('')
+    const passwordRef = useRef('')
+
+    const handleSubmit = event => {
+        event.preventDefault()
+    }
+
+
     return (
         <div className='container w-50 mx-auto'>
             <h2 className='text-center'>Please Login</h2>
 
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -17,7 +27,7 @@ const Login = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
--- src/note.jsx
+++ src/note.jsx
@@ -2,5 +2,8 @@
  * react useRef -
  * useRef hook holo amon ektu hook ja ekti mutable referance object return kore.
  * 
+ * ref ta use korar time a ( RefName.current )
+ * ayta jekhane set korbo i menan jar jonne use korbo, oikhane ( ref={RefName} )
+ * 
  * useRef is like something useParams or useNavigate.
 */

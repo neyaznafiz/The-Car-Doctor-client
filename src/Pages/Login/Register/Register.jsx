9914108449467c2437f05d208456659c6c0a2a4e
niiyazobug <niiyaz5555@gmail.com>
2022-04-13 15:47:58 +0600

    back to login from register section navigation adde

--- src/Pages/Login/Register/Register.jsx
+++ src/Pages/Login/Register/Register.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './Register.css'
 
 const Register = () => {
+
+    const navigate = useNavigate()
+
+
+    const navigateLogin = event => {
+        navigate('/login')
+    }
+
     return (
         <div className='register-form'>
-            <h2 style={{textAlign:'center'},{marginTop:'40px'}}>Please Register</h2>
+            <h2 style={{ textAlign: 'center' }, { marginTop: '40px' }}>Please Register</h2>
 
             <form>
-                <input type="text" name='name' placeholder='Your Name'/>
-                
-                <input type="email" name="email" id="" placeholder='Email' required/>
-                
-                <input type="password" name="password" id="" placeholder='Password' required/>
+                <input type="text" name='name' placeholder='Your Name' />
+
+                <input type="email" name="email" id="" placeholder='Email' required />
+
+                <input type="password" name="password" id="" placeholder='Password' required />
 
                 <input type="submit" value="Register" />
             </form>
+
+            <p> Already have an account <Link to='/login' onClick={navigateLogin} className='text-secondary fw-bold text-decoration-none'  >Please Login</Link></p>
+
         </div>
     );
 };

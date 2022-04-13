4a51685e219dd169159a5088dd2057fa7d6a4599
niiyazobug <niiyaz5555@gmail.com>
2022-04-13 15:44:06 +0600

    css added in register form

--- src/Pages/Login/Register/Register.jsx
+++ src/Pages/Login/Register/Register.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import './Register.css'
 
 const Register = () => {
     return (
-        <div>
-            <h2>Register</h2>
+        <div className='register-form'>
+            <h2 style={{textAlign:'center'},{marginTop:'40px'}}>Please Register</h2>
+
+            <form>
+                <input type="text" name='name' placeholder='Your Name'/>
+                
+                <input type="email" name="email" id="" placeholder='Email' required/>
+                
+                <input type="password" name="password" id="" placeholder='Password' required/>
+
+                <input type="submit" value="Register" />
+            </form>
         </div>
     );
 };

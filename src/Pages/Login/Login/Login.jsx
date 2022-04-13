5741bb358bd5f4366ac8e90e2309e39633f59518
niiyazobug <niiyaz5555@gmail.com>
2022-04-13 15:04:29 +0600

    use email and paas ref in handle submit function button

--- src/Pages/Login/Login/Login.jsx
+++ src/Pages/Login/Login/Login.jsx
@@ -9,6 +9,11 @@ const Login = () => {
 
     const handleSubmit = event => {
         event.preventDefault()
+
+        const email =emailRef.current.value
+        const password = passwordRef.current.value
+
+        console.log(email, password);
     }
 
 
@@ -19,7 +24,7 @@ const Login = () => {
             <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -27,7 +32,7 @@ const Login = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control ref={passwordRef} type="password" placeholder="Password" />
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />

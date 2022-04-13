290c8f937ff13a686b32bdd536d1e5bfe13b48bc
niiyazobug <niiyaz5555@gmail.com>
2022-04-13 20:52:32 +0600

    note edited

--- src/note.jsx
+++ src/note.jsx
@@ -1,6 +1,6 @@
 /**
  * react useRef -
- * useRef hook holo amon ektu hook ja ekti mutable referance object return kore.
+ * useRef hook holo amon ekta hook ja ekti mutable referance object return kore.
  * 
  * ref ta use korar time a ( RefName.current )
  * ayta jekhane set korbo i menan jar jonne use korbo, oikhane ( ref={RefName} )

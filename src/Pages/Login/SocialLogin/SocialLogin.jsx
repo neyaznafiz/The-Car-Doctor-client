2cd8a894335cdf8409f2c62f72cc76915e1a4f49
niiyazobug <niiyaz5555@gmail.com>
2022-04-14 15:08:41 +0600

    github  signin method added

--- src/Pages/Login/SocialLogin/SocialLogin.jsx
+++ src/Pages/Login/SocialLogin/SocialLogin.jsx
@@ -3,7 +3,7 @@ import google from '../../../Images/Social/google-logo.png'
 import facebook from '../../../Images/Social/facebook.png'
 import twitter from '../../../Images/Social/twitter.png'
 import github from '../../../Images/Social/github.png'
-import { useSignInWithGoogle, useSignInWithTwitter } from 'react-firebase-hooks/auth';
+import { useSignInWithGithub, useSignInWithGoogle, useSignInWithTwitter } from 'react-firebase-hooks/auth';
 import auth from '../../../Firebase/firebase.init';
 import { useNavigate } from 'react-router-dom';
 
@@ -12,19 +12,21 @@ const SocialLogin = () => {
 
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
 
-    const [signInWithTwitter] = useSignInWithTwitter(auth);
+    const [signInWithTwitter, user3, loading3, error3] = useSignInWithTwitter(auth);
 
     const navigate = useNavigate()
 
+    const [signInWithGithub, user4, loading4, error4] = useSignInWithGithub(auth);
+
 
     let errorElement
-    if (error) {
+    if (error || error3 || error4) {
         errorElement = <div>
-            <p className='text-danger'>Error: {error.message}</p>
+            <p className='text-danger'>Error: {error?.message} {error3?.message} {error4?.message}</p>
         </div>
     }
 
-    if (user) {
+    if (user || user3 || user4) {
         navigate('/home')
     }
 
@@ -54,12 +56,12 @@ const SocialLogin = () => {
                     <span className='pt-1'> Facebook Sign In</span>
                 </button>
 
-                <button onClick={()=>signInWithTwitter()} className='btn btn-dark w-25 d-block py-2 mx-auto rounded-pill'>
+                <button onClick={() => signInWithTwitter()} className='btn btn-dark w-25 d-block py-2 mx-auto rounded-pill'>
                     <img style={{ width: '36px' }} src={twitter} alt="" />
                     <span className='px-2 pt-1'>Twitter Sign In</span>
                 </button>
 
-                <button className='btn btn-dark w-25 d-block py-2 my-2 mx-auto rounded-pill'>
+                <button onClick={() => signInWithGithub()} className='btn btn-dark w-25 d-block py-2 my-2 mx-auto rounded-pill'>
                     <img style={{ width: '28px' }} src={github} alt="" />
                     <span className='px-2 pt-1'>Github Sign In</span>
                 </button>

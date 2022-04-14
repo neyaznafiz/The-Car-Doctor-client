bb335b34e96a6a8dbb6908c29d7ffc1c2742006d
niiyazobug <niiyaz5555@gmail.com>
2022-04-14 12:44:13 +0600

    Social login component added

---
+++ src/Pages/Login/SocialLogin/SocialLogin.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SocialLogin = () => {
+    return (
+        <div>
+            <div></div>
+            <p>or</p>
+            <div></div>
+        </div>
+    );
+};
+
+export default SocialLogin;

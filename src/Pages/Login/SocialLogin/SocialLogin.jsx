879228940bb2074975ae751c40800a602daf16b5
niiyazobug <niiyaz5555@gmail.com>
2022-04-14 14:34:59 +0600

    twitter  signin method added

--- src/Pages/Login/SocialLogin/SocialLogin.jsx
+++ src/Pages/Login/SocialLogin/SocialLogin.jsx
@@ -3,7 +3,7 @@ import google from '../../../Images/Social/google-logo.png'
 import facebook from '../../../Images/Social/facebook.png'
 import twitter from '../../../Images/Social/twitter.png'
 import github from '../../../Images/Social/github.png'
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithGoogle, useSignInWithTwitter } from 'react-firebase-hooks/auth';
 import auth from '../../../Firebase/firebase.init';
 import { useNavigate } from 'react-router-dom';
 
@@ -11,6 +11,8 @@ const SocialLogin = () => {
 
 
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+    const [signInWithTwitter] = useSignInWithTwitter(auth);
 
     const navigate = useNavigate()
 
@@ -52,7 +54,7 @@ const SocialLogin = () => {
                     <span className='pt-1'> Facebook Sign In</span>
                 </button>
 
-                <button className='btn btn-dark w-25 d-block py-2 mx-auto rounded-pill'>
+                <button onClick={()=>signInWithTwitter()} className='btn btn-dark w-25 d-block py-2 mx-auto rounded-pill'>
                     <img style={{ width: '36px' }} src={twitter} alt="" />
                     <span className='px-2 pt-1'>Twitter Sign In</span>
                 </button>

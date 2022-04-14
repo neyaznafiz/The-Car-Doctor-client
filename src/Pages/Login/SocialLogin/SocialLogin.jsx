b2687c4f9ffbdeedd848366284e80ea807f8b960
niiyazobug <niiyaz5555@gmail.com>
2022-04-14 13:53:35 +0600

    facebook, twitter, github button added

--- src/Pages/Login/SocialLogin/SocialLogin.jsx
+++ src/Pages/Login/SocialLogin/SocialLogin.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import google from '../../../Images/Social/google-logo.png'
+import facebook from '../../../Images/Social/facebook.png'
+import twitter from '../../../Images/Social/twitter.png'
+import github from '../../../Images/Social/github.png'
 
 const SocialLogin = () => {
     return (
@@ -12,7 +16,27 @@ const SocialLogin = () => {
 
             </div>
 
-            <button className='btn btn-light w-50'> Google Sign In</button>
+            <div>
+                <button className='btn btn-dark w-25 d-block py-2 mx-auto rounded-pill'>
+                    <img style={{ width: '28px' }} src={google} alt="" />
+                    <span className='px-2 pt-1'>Google Sign In</span>
+                </button>
+
+                <button className='btn btn-dark w-25 d-block py-2 my-2 mx-auto rounded-pill'>
+                    <img style={{ width: '34px' }} src={facebook} alt="" />
+                    <span className='pt-1'> Facebook Sign In</span>
+                </button>
+
+                <button className='btn btn-dark w-25 d-block py-2 mx-auto rounded-pill'>
+                    <img style={{ width: '36px' }} src={twitter} alt="" />
+                    <span className='px-2 pt-1'>Twitter Sign In</span>
+                </button>
+
+                <button className='btn btn-dark w-25 d-block py-2 my-2 mx-auto rounded-pill'>
+                    <img style={{ width: '28px' }} src={github} alt="" />
+                    <span className='px-2 pt-1'>Github Sign In</span>
+                </button>
+            </div>
 
         </div>
 

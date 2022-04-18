d44125d1c9a92c6cc285ee53e4d43303d8b03a68
niiyazobug <niiyaz5555@gmail.com>
2022-04-19 03:05:06 +0600

    Helmet use in home and about section

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 import Banner from '../Banner/Banner';
 import Experts from '../Experts/Experts';
 import Services from '../Services/Services';
@@ -6,6 +7,11 @@ import Services from '../Services/Services';
 const Home = () => {
     return (
         <>
+
+<Helmet>
+    <title>Home - The Car Doctor</title>
+</Helmet>
+
             <Banner></Banner>
             <Services></Services>
             <Experts></Experts>

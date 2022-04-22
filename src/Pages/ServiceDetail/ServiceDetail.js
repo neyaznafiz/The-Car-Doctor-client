a283648245cb147d978060172c81a4183e230a96
niiyazobug <niiyaz5555@gmail.com>
2022-04-22 16:29:37 +0600

    data fetch in sericedetails and display service name

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
 const ServiceDetail = () => {
 
     const { serviceId } = useParams()
 
+    const [service, setService] = useState({})
+
+    useEffect(() => {
+        const url = `http://localhost:5000/service/${serviceId}`
+
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setService(data))
+    }, [])
+
     return (
         <div>
-            <h2>this is service detail : {serviceId}</h2>
+            <h2>You are about to book: {service.name}</h2>
 
             <Link to='/checkout'>
 
                 <div className='text-center'>
-                <button className='btn btn-dark'> Proceed Checkout</button>
+                    <button className='btn btn-dark'> Proceed Checkout</button>
                 </div>
             </Link>
         </div>

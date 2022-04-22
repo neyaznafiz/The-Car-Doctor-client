e537a9a54ad17f15de3042996aaae49989dac65d
niiyazobug <niiyaz5555@gmail.com>
2022-04-22 16:40:00 +0600

    email verification condition edited just for password

--- src/Pages/Login/RequireAuth/RequireAuth.jsx
+++ src/Pages/Login/RequireAuth/RequireAuth.jsx
@@ -19,7 +19,7 @@ const RequireAuth = ({ children }) => {
         return <Navigate to="/login" state={{ from: location }} replace />
     }
 
-    if (!user.emailVerified) {
+    if (user.providerData[0]?.providerId === 'password' && !user.emailVerified) {
         return <div>
             <h3 className='text-danger'>Your Email is not verified!!</h3>
             <h5 className='text-success'>Please verify your email address</h5>

10f942f28ae800bec222bcc1334241c3ed6f3bbe
niiyazobug <niiyaz5555@gmail.com>
2022-04-22 16:47:29 +0600

    created addservice component

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Login from './Pages/Login/Login/Login';
 import Register from './Pages/Login/Register/Register';
 import CheckOut from './Pages/CheckOut/CheckOut/CheckOut';
 import RequireAuth from './Pages/Login/RequireAuth/RequireAuth';
+import AddService from './Pages/AddService/AddService';
 
 function App() {
   return (
@@ -22,23 +23,22 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         
         <Route path='/home' element={<Home></Home>}></Route>
-
         <Route path='/service/:serviceId' element={<ServiceDetail></ServiceDetail>}></Route>
-
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
 
-
-
         <Route path='/checkout' element={
-
           <RequireAuth>
             <CheckOut></CheckOut>
           </RequireAuth>
-
         }></Route>
 
+        <Route path='/addservice' element={
+          <RequireAuth>
+            <AddService></AddService>
+          </RequireAuth>
+        }></Route>
 
         <Route path='*' element={<NotFound></NotFound>}></Route>
 

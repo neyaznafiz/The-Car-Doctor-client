186d8ec4cf6310c7e583e4405a191a3e2a80e0b9
niiyazobug <niiyaz5555@gmail.com>
2022-04-26 01:25:08 +0600

    create a hook for serviceDetils and use it checkout and shou product name

--- src/Pages/CheckOut/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut/CheckOut.js
@@ -1,11 +1,20 @@
- import React from 'react';
- 
- const CheckOut = () => {
-     return (
-         <div>
-             <h2>Please Checkout your Booking</h2>
-         </div>
-     );
- };
- 
- export default CheckOut;
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import useServiceDetail from '../../../Hooks/useServiceDetail';
+
+const CheckOut = () => {
+
+    const { serviceId } = useParams()
+
+    const [service] = useServiceDetail(serviceId)
+
+    return (
+        <div className='w-50 mx-auto'>
+            <h2>Please order: {service.name}</h2>
+
+           
+        </div>
+    );
+};
+
+export default CheckOut;

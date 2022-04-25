99044a8398532c1d4038bf6368133ed02431fbba
niiyazobug <niiyaz5555@gmail.com>
2022-04-26 01:37:23 +0600

    form created in checkout and show dynamically service name

--- src/Pages/CheckOut/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut/CheckOut.js
@@ -12,7 +12,19 @@ const CheckOut = () => {
         <div className='w-50 mx-auto'>
             <h2>Please order: {service.name}</h2>
 
-           
+            <form>
+                <input className='w-100 mb-2' type="text" name='name' placeholder='Name' required/>
+                <br />
+                <input className='w-100 mb-2' type="email" name='email' placeholder='Email' required/>
+                <br />
+                <input className='w-100 mb-2' type="text" name='service' value={service.name} placeholder='Service' required/>
+                <br />
+                <input className='w-100 mb-2' type="text" name='address' placeholder='Address' required/>
+                <br />
+                <input className='w-100 mb-2' type="text" name='phone' placeholder='Phone' required/>
+                <br />
+                <input className='btn btn-dark' type="submit" value='Place Order' />
+            </form>
         </div>
     );
 };

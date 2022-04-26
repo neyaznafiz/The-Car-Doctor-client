451285a423b2af664559ceeac863715b29e2156f
niiyazobug <niiyaz5555@gmail.com>
2022-04-26 13:44:17 +0600

    jwt token added in login

--- src/Pages/Login/Login/Login.jsx
+++ src/Pages/Login/Login/Login.jsx
@@ -8,6 +8,7 @@ import Loading from '../../../Shared/Loading/Loading';
 import SocialLogin from '../SocialLogin/SocialLogin';
 import { toast } from 'react-toastify';
 import PageTitle from '../../../Shared/PageTitle/PageTitle';
+import axios from 'axios';
 
 const Login = () => {
 
@@ -46,16 +47,19 @@ const Login = () => {
 
 
     if (user) {
-        navigate(from, { replace: true });
+        // navigate(from, { replace: true });
     }
 
-    const handleSubmit = event => {
+    const handleSubmit = async event => {
         event.preventDefault()
 
         const email = emailRef.current.value
         const password = passwordRef.current.value
 
-        signInWithEmailAndPassword(email, password)
+        await signInWithEmailAndPassword(email, password)
+        const { data } = await axios.post('http://localhost:5000/login', { email })
+        localStorage.setItem('accessToken', data.accessToken)
+        navigate(from, { replace: true })
     }
 
     const resetPassword = async () => {
